9df42f4f0641aedc3f18ba4cbc7da6d636baec86
ntomeniko <domenico.espinosa@gmail.com>
2020-12-04 20:12:22 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mi proyecto de Data Science
+
+Este es un archivo de ayuda con información sobre este proyecto
---
+++ test.py
@@ -0,0 +1,8 @@
+print('HOLA BUNDO!')
+
+d={}
+
+d["app"] = "apples - apples - bin"
+d["bin"] = "bins  - bins  - bin"
+d["dog"] = "dogs - dogs - bin"
+d["ele"] = "elephants"
